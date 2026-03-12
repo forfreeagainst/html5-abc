4dda4ea09b6af5beccb6417b20ae18db7cfe3dac
forfreeagainst <1982740368@qq.com>
2026-03-13 07:33:31 +0800

    feat: react-router-dom图片

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { DndProvider, useDrag, useDrop } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { Input } from 'antd';
 
 function App() {
+  const [name, setName] = useState('durant')
+  const log = () => {
+    console.log(name, '???')
+  }
   return (
     <>
      <DndProvider backend={HTML5Backend}>
-        <p>充电宝</p>
+        <p>充电宝{name}</p>
+        <Input placeholder="Basic usage"
+        onPressEnter={log} 
+        value={name} onChange={
+          (e) => {setName(e.target.value)}
+        }/>
      </DndProvider>
     </>
   )
